68a2c86c6d5e02808a71ada65dcee075337f45e1
Juan David Cardona <jdavidcardonam@gmail.com>
2021-01-02 00:37:34 -0500

    creacion de la funcion printFuelSold

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,14 @@
-let fuelTank = document.getElementById("fuelTank");
+// const printFuelSold = (valueFuel) => {
+//   let x = parseFloat(valueFuel.value);
+//   console.log(x);
+// };
 
+function printFuelSold(valueFuel) {
+  let num = parseInt(valueFuel.value);
+  console.log(num);
+}
+
+let valueFuel = document.getElementById("fuelSold");
+let submitFuel = document.getElementById("submitFuel");
+submitFuel.addEventListener("click", printFuelSold);
 
