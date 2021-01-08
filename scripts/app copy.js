e02e3fe36d37b7feb74ba632c7aeab515b250fd7
Juan David Cardona <jdavidcardonam@gmail.com>
2021-01-07 23:46:52 -0500

    Final test - approved for merge

---
+++ scripts/app copy.js
@@ -0,0 +1,150 @@
+//variables for Fuel
+const submitFuel = document.getElementById("submitFuel"); //button Sell
+submitFuel.addEventListener("click", fuelButton);
+const fuelSoldList = []; //Array for fuel sold
+
+//variables for Diesel
+const submitDiesel = document.getElementById("submitDiesel"); //button Sell
+submitDiesel.addEventListener("click", dieselButton);
+const dieselSoldList = []; //Array for diesel sold
+
+function fuelButton() {
+    const soldList = fuelSoldList;
+    const maxCapacity = 4922.39;
+
+    actionButton(soldList, maxCapacity);
+}
+
+function dieselButton() {
+    const soldList = dieselSoldList;
+    const maxCapacity = 1603.7;
+    actionButton(soldList, maxCapacity);
+}
+
+//function to save all transactions in the array
+const saveTransactionSold = (soldList, maxCapacity) => {
+    const valueFuel = parseFloat(document.getElementById("fuelSold").value);
+    const valueDiesel = parseFloat(document.getElementById("dieselSold").value);
+    if (soldList == fuelSoldList) {
+        //Get list name
+        if (isValid(fuelSoldList, maxCapacity, valueFuel) === 1) {
+            //value is not empty
+            //Value is a number then push in array
+            fuelSoldList.push(valueFuel);
+            console.log(fuelSoldList);
+        }
+    } else {
+        if (isValid(dieselSoldList, maxCapacity, valueDiesel) === 1) {
+            //value is not empty
+            //Value is a number then push in array
+            dieselSoldList.push(valueDiesel);
+            console.log(dieselSoldList);
+        }
+    }
+};
+
+//function to delete all content in the div that shows last five transactions
+const deleteSoldList = (soldList) => {
+    if (soldList == fuelSoldList) {
+        document.getElementById("listFuelSold").innerHTML = "";
+    } else {
+        document.getElementById("listDieselSold").innerHTML = "";
+    }
+};
+
+//function print las five transactions
+const printLastFiveSold = (soldList) => {
+    const printList = soldList.slice(-5).reverse();
+    printList.forEach((element) => {
+        // Prints last five child nodes
+        const node = document.createElement("li");
+        const textnode = document.createTextNode(element + " gallons");
+        node.appendChild(textnode);
+        if (soldList == fuelSoldList) {
+            document.getElementById("listFuelSold").appendChild(node);
+        } else {
+            document.getElementById("listDieselSold").appendChild(node);
+        }
+    });
+};
+
+//function to clear text box
+const clearTextBox = (soldList) => {
+    if (soldList == fuelSoldList) {
+        document.getElementById("fuelSold").value = ""; //delete value of input text box
+    } else {
+        document.getElementById("dieselSold").value = ""; //delete value of input text box
+    }
+};
+
+//sum array sold list
+const sumSoldList = (soldList) => {
+    return soldList.reduce(function (a, b) {
+        return a + b;
+    }, 0);
+};
+
+//get actual value of tank, substraction sum sold list of max capacity
+const actualValue = (maxCapacity, sumSoldList) => {
+    return maxCapacity - sumSoldList;
+};
+
+//get porcentage value --  actual value / max capacity
+const porcentageValue = (maxCapacity, actualValue) => {
+    return (actualValue / maxCapacity) * 100;
+};
+
+//Modify vetical bar size in the  tanks
+const modifyVerticalBar = (soldList, porcentageValue) => {
+    const barFuelTank = document.getElementById("barFuelTank"); //id vertical bar
+    const barDieselTank = document.getElementById("barDieselTank"); //id vertical bar
+    if (soldList == fuelSoldList) {
+        barFuelTank.style.height = `${porcentageValue}%`;
+    } else {
+        barDieselTank.style.height = `${porcentageValue}%`;
+    }
+};
+
+//function to print actual value and porcentage value above tank
+const printInfoAboveTank = (soldList, actualValue, porcentageValue) => {
+    if (soldList == fuelSoldList) {
+        const infoTank = document.getElementById("porcentageFuel");
+        infoTank.innerText = `${parseFloat(actualValue).toFixed(2)} gallons ${parseFloat(porcentageValue).toFixed(2)}%`;
+    } else {
+        const infoTank = document.getElementById("porcentageDiesel");
+        infoTank.innerText = `${parseFloat(actualValue).toFixed(2)} gallons ${parseFloat(porcentageValue).toFixed(2)}%`;
+    }
+};
+
+//function to validate value input is greater than zero and less than actual capacity
+function isValid(soldList, maxCapacity, value) {
+    const actualCapacity = actualValue(maxCapacity, sumSoldList(soldList));
+
+    if (!isNaN(value)) {
+        if (value > 0) {
+            if (value > actualCapacity) {
+                alert("It´s not enough in the tank");
+            } else {
+                return 1;
+            }
+        } else {
+            alert("It must be greater than zero");
+        }
+    }
+}
+
+//function to make actions button
+function actionButton(type, capacity) {
+    const soldList = type;
+    const maxCapacity = capacity;
+    saveTransactionSold(soldList, maxCapacity);
+    deleteSoldList(soldList);
+    printLastFiveSold(soldList);
+    clearTextBox(soldList);
+    const sum = sumSoldList(soldList);
+    const actual = actualValue(maxCapacity, sum);
+    const porcentage = porcentageValue(maxCapacity, actual);
+    modifyVerticalBar(soldList, porcentage);
+    printInfoAboveTank(soldList, actual, porcentage);
+    console.log(sum, actual, `${porcentage}%`);
+}
--- scripts/app.js
+++ scripts/app.js
@@ -1,159 +1,71 @@
-//variables for Fuel
-const submitFuel = document.getElementById("submitFuel"); //button Sell
-submitFuel.addEventListener("click", fuelButton);
-const fuelSoldList = []; //Array for fuel sold
-
-//variables for Diesel
-const submitDiesel = document.getElementById("submitDiesel"); //button Sell
-submitDiesel.addEventListener("click", dieselButton);
-const dieselSoldList = []; //Array for diesel sold
-
-function fuelButton() {
-  const soldList = fuelSoldList;
-  const maxCapacity = 4922.39;
-
-  actionButton(soldList, maxCapacity);
+class Fuel {
+    constructor(nameFuel, Capacity) {
+        this.nameFuel = nameFuel;
+        this.Capacity = Capacity;
+        this.Transactions = [];
+    }
+    saveTransactions = (value) => this.Transactions.push(value);
+    actualValue = () => this.Capacity - this.Transactions.reduce((a, b) => a + b, 0);
+    porcentageValue = () => (this.actualValue() / this.Capacity) * 100;
 }
 
-function dieselButton() {
-  const soldList = dieselSoldList;
-  const maxCapacity = 1603.70;
-  actionButton(soldList, maxCapacity);
-}
+//Variables for gas fuel
+const Gas = new Fuel("Gas", 4922.39); // new object
+const gasValue = document.getElementById("gasSold"); //input value sold
+const gasPrintList = document.getElementById("listGasSold"); // container html print gas list
+const gasVerticalBarContainer = document.getElementById("barGasTank"); //id vertical bar
+const sellGas = document.getElementById("submitGas"); //button sell gas
+const gasInfoTank = document.getElementById("porcentageGas"); //Info above Gas tank
+sellGas.addEventListener("click", function () {
+    Button(Gas, gasValue, gasPrintList, gasVerticalBarContainer, gasInfoTank);
+});
 
-//function to save all transactions in the array
-const saveTransactionSold = (soldList, maxCapacity) => {
-  const valueFuel = parseFloat(document.getElementById("fuelSold").value);
-  const valueDiesel = parseFloat(document.getElementById("dieselSold").value);
-  if (soldList == fuelSoldList) {
-    //Get list name
-    if (isValid(fuelSoldList, maxCapacity, valueFuel) === 1) {
-      //value is not empty
-      //Value is a number then push in array
-      fuelSoldList.push(valueFuel);
-      console.log(fuelSoldList);
-    }
-  } else {
-    if (isValid(dieselSoldList, maxCapacity, valueDiesel) === 1) {
-      //value is not empty
-      //Value is a number then push in array
-      dieselSoldList.push(valueDiesel);
-      console.log(dieselSoldList);
-    }
-  }
-};
+//Variables for Diesel fuel
+const Diesel = new Fuel("Diesel", 1603.7); //new object
+const dieselValue = document.getElementById("dieselSold"); //input value sold
+const dieselPrintList = document.getElementById("listDieselSold"); // container html print gas list
+const dieselVerticalBarContainer = document.getElementById("barDieselTank"); //id vertical bar
+const sellDiesel = document.getElementById("submitDiesel"); //button sell gas
+const dieselInfoTank = document.getElementById("porcentageDiesel"); //Info above Gas tank
+sellDiesel.addEventListener("click", function () {
+    Button(Diesel, dieselValue, dieselPrintList, dieselVerticalBarContainer, dieselInfoTank);
+});
 
-
-//function to delete all content in the div that shows last five transactions
-const deleteSoldList = (soldList) => {
-  if (soldList == fuelSoldList) {
-    document.getElementById("listFuelSold").innerHTML = "";
-  } else {
-    document.getElementById("listDieselSold").innerHTML = "";
-  }
-};
-//function print las five transactions
-const printLastFiveSold = (soldList) => {
-  const printList = soldList.slice(-5).reverse();
-  printList.forEach((element) => {
-    // Prints last five child nodes
-    const node = document.createElement("li");
-    const textnode = document.createTextNode(element + " gallons");
-    node.appendChild(textnode);
-    if (soldList == fuelSoldList) {
-      document.getElementById("listFuelSold").appendChild(node);
-    } else {
-      document.getElementById("listDieselSold").appendChild(node);
-    }
-  });
-};
-
-//function to clear text box
-const clearTextBox = (soldList) => {
-  if (soldList == fuelSoldList) {
-    document.getElementById("fuelSold").value = ""; //delete value of input text box
-  } else {
-    document.getElementById("dieselSold").value = ""; //delete value of input text box
-  }
-};
-
-//sum array sold list
-const sumSoldList = (soldList) => {
-  return soldList.reduce(function (a, b) {
-    return a + b;
-  }, 0);
-};
-
-//get actual value of tank, substraction sum sold list of max capacity
-const actualValue = (maxCapacity, sumSoldList) => {
-  return maxCapacity - sumSoldList;
-};
-
-//get porcentage value --  actual value / max capacity
-const porcentageValue = (maxCapacity, actualValue) => {
-  return (actualValue / maxCapacity) * 100;
-};
-
-//Modify vetical bar size in the  tanks
-const modifyVerticalBar = (soldList, porcentageValue) => {
-  const barFuelTank = document.getElementById("barFuelTank"); //id vertical bar
-  const barDieselTank = document.getElementById("barDieselTank"); //id vertical bar
-  if (soldList == fuelSoldList) {
-    barFuelTank.style.height = `${porcentageValue}%`;
-  } else {
-    barDieselTank.style.height = `${porcentageValue}%`;
-  }
-};
-
-//function to print actual value and porcentage value above tank
-const printInfoAboveTank = (soldList, actualValue, porcentageValue) => {
-  if (porcentageValue < 30) {
-    ("alert!");
-  }
-
-  if (soldList == fuelSoldList) {
-    const infoTank = document.getElementById("porcentageFuel");
-    infoTank.innerText = `${parseFloat(actualValue).toFixed(
-      2
-    )} gallons ${parseFloat(porcentageValue).toFixed(2)}%`;
-  } else {
-    const infoTank = document.getElementById("porcentageDiesel");
-    infoTank.innerText = `${parseFloat(actualValue).toFixed(
-      2
-    )} gallons ${parseFloat(porcentageValue).toFixed(2)}%`;
-  }
-};
-
-//function to validate value input is greater than zero and less than actual capacity
-function isValid(soldList, maxCapacity, value) {
-  const actualCapacity = actualValue(maxCapacity, sumSoldList(soldList));
-
-  if (!isNaN(value)) {
-    if (value > 0) {
-      if (value > actualCapacity) {
+const isValid = function (fuel, value) {
+    if (isNaN(value)) {
+        return false;
+    } else if (value <= 0) {
+        alert("It must be greater than zero");
+        return false;
+    } else if (value > fuel.actualValue()) {
         alert("It´s not enough in the tank");
-      } else {
-        return 1;
-      }
+        return false;
     } else {
-      alert("It must be greater than zero");
+        return true;
     }
-  }
-}
+};
 
-//function to make actions button
-function actionButton(type, capacity) {
-  const soldList = type;
-  const maxCapacity = capacity;
+const printLastTransactions = function (fuel, lastNumberTransactions, printListContainer) {
+    printListContainer.innerHTML = "";
+    const printList = fuel.Transactions.slice(-lastNumberTransactions).reverse();
+    printList.forEach((element) => {
+        const node = document.createElement("li");
+        const textnode = document.createTextNode(`${element} gallons`);
+        node.appendChild(textnode);
+        printListContainer.appendChild(node);
+    });
+};
 
-  saveTransactionSold(soldList, maxCapacity);
-  deleteSoldList(soldList);
-  printLastFiveSold(soldList);
-  clearTextBox(soldList);
-  const sum = sumSoldList(soldList);
-  const actual = actualValue(maxCapacity, sum);
-  const porcentage = porcentageValue(maxCapacity, actual);
-  modifyVerticalBar(soldList, porcentage);
-  printInfoAboveTank(soldList, actual, porcentage);
-  console.log(sum, actual, `${porcentage}%`);
+const round = (value, decimals) => Number(Math.round(value + "e" + decimals) + "e-" + decimals);
+
+function Button(fuel, fuelValueString, printListContainer, verticalBarContainer, infoTank) {
+    let fuelValue = parseFloat(fuelValueString.value);
+    if (!isValid(fuel, fuelValue)) {
+        return;
+    }
+    fuel.saveTransactions(fuelValue);
+    fuelValueString.value = ""; //clear input text box
+    printLastTransactions(fuel, 5, printListContainer);
+    verticalBarContainer.style.height = `${fuel.porcentageValue()}%`; //modifyVerticalBar
+    infoTank.innerText = `${round(fuel.actualValue(), 2)} gallons ${round(fuel.porcentageValue(), 2)}%`; //Print info tank
 }
